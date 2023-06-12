b8fdeeea6180857230e2cf26148b2921cd91a776
darina1009 <darina_1009@mail.ru>
2023-06-12 18:05:28 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿int num = ReadLine();
+int sum = Sum(num);
+
+Console.WriteLine(sum);
+
+int ReadLine(){
+    Console.WriteLine("Введите число");
+    string input = Console.ReadLine();
+    int num = Convert.ToInt32(input);
+    return num;
+}
+
+int Sum (int num){
+     int sum = 0;
+     
+    for ( int i = 0; i <  num ; i++){
+       
+        sum += num % 10; 
+        num = num / 10;
+    }
+    return sum;
+}
+
+
+
