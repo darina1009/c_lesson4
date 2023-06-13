cc1e531ecd0c8d460da7a2e9eb29ebb57d874793
darina1009 <darina_1009@mail.ru>
2023-06-13 13:49:27 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int num = ReadLine();
+int stepen = Convert.ToInt32(Console.ReadLine());
+int pow = Power(num, stepen);
+Console.WriteLine($"Степень {pow}");
+
+
+int ReadLine(){
+    Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите степень");
+  
+    return num;
+}
+
+int Power(double num, double stepen){
+     int pow = Convert.ToInt32(Math.Pow(num,stepen ));
+
+    return pow;
+}
