774bf472bbf618b2e771db6865057bfc2ac4b5be
darina1009 <darina_1009@mail.ru>
2023-06-13 17:16:46 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+RandomArray();
+
+int [] RandomArray(){
+int [] array = new int[8];
+for (int a = 0; a < array.Length; a++){
+    array[a] = new Random().Next(100);
+    Console.WriteLine(array[a]);
+}
+return array;
+}
